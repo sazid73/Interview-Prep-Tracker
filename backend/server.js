9d460c8314c5b16f0619b4a1c6137e2ce1f9c932
Sazid Amin <sazid15730551@gmail.com>
2026-06-05 02:55:01 +0100

    prep

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Socket-ID');
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
